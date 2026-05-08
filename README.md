03fae6760f57500a2ac1a8246fd2dd446c754852
Wesley Fraga <wesleyrodriguesfraga14@gmail.com>
2026-05-08 15:39:14 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Controle Financeiro
+
+Projeto desenvolvido como desafio prático do curso no SENAC.
+
+O objetivo do desafio era criar toda a lógica em JavaScript para um sistema simples de controle financeiro, utilizando um HTML e CSS previamente disponibilizados pelo professor.
+
+---
+
+🔗 **Projeto no ar:** https://wsfraga.github.io/controle-financeiro/
+
+## Preview
+
+```bash
+img/preview.png
+```
+
+---
+
+## Funcionalidades
+
+* Adicionar novos gastos
+* Atualizar quantidade de gastos automaticamente
+* Calcular total dos gastos em tempo real
+* Remover gastos da lista
+* Formatação automática de moeda em Real (R$)
+* Interface responsiva
+
+---
+
+## Tecnologias utilizadas
+
+* HTML5
+* CSS3
+* JavaScript
+
+---
+
+## Aprendizados
+
+Durante o desenvolvimento desse projeto, pratiquei:
+
+* Manipulação do DOM
+* Eventos com `addEventListener`
+* Criação dinâmica de elementos
+* Uso de funções para separar responsabilidades
+* Manipulação de strings e números
+* Uso de `querySelector` e `querySelectorAll`
+* Atualização dinâmica da interface
+* Event Delegation para remoção de itens
+* Formatação de moeda utilizando `Intl.NumberFormat`
+
+---
+
+## Estrutura do projeto
+
+```bash
+📁 controle-financeiro
+ ┣ 📁 img
+ ┣ 📄 index.html
+ ┣ 📄 styles.css
+ ┗ 📄 scripts.js
+```
+
+---
+
+## Como executar o projeto
+
+1. Clone o repositório:
+
+```bash
+git clone https://github.com/seu-usuario/seu-repositorio.git
+```
+
+2. Abra o arquivo `index.html` no navegador.
+
+---
+
+## Melhorias futuras
+
+* Salvar gastos no LocalStorage
+* Editar gastos
+* Filtrar por categoria
+* Gráficos financeiros
+* Dark mode
+* Validação mais avançada dos campos
+
+---
+
+## 👨‍💻 Autor
+
+**Wesley da Silva Rodrigues Fraga**
+
+- GitHub: https://github.com/wsfraga
+- LinkedIn: [www.linkedin.com/in/wesley-fraga-188384392](https://www.linkedin.com/in/wesley-fraga-188384392/)
+
+---
---
+++ scripts.js
@@ -0,0 +1,130 @@
+const valorTotal = document.querySelector('[data-gastos="total"]');
+const form = document.getElementById('input-reset');
+const valorDoGasto = document.querySelector('[data-gastos="valor"]');
+const btnSubmit = document.querySelector('[data-gastos="btn-submit"]');
+const nomeDoGasto = document.querySelector('[data-gastos="spend"]');
+const categoria = document.querySelector('[data-gastos="categoria"]');
+const lista = document.querySelector('[data-gastos="lista"]');
+const valorDaQuantidade = document.querySelector('[data-gastos="quantidade"]');
+
+
+// ---------- Formatacao da moeda -------------
+
+const formatador = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL'
+})
+
+
+// ---------- restricao no valor digitado -----------
+
+valorDoGasto.addEventListener('input', (event) => {
+  let valor = valorDoGasto.value.replace(/\D/g, "");
+  valor = (valor / 100).toFixed(2);
+
+  if (valor === '0,00') {
+    event.target.value = '';
+  } else {
+    event.target.value = formatador.format(valor);
+  }
+});
+
+
+// --------- adicionarGasto --------
+
+function adicionarGasto() {
+
+  btnSubmit.addEventListener('click', () => {
+
+    const gasto = {
+      nome: nomeDoGasto.value,
+      categoria: categoria.value,
+      valor: valorDoGasto.value,
+    }
+
+    if (gasto.nome === '') return;
+    if (gasto.categoria === 'Selecione' || gasto.categoria === '') return;
+    if (gasto.valor === '') return;
+
+    atualizaLista(gasto);
+    form.reset();
+  })
+}
+
+adicionarGasto();
+
+
+// ----- atualizaLista ------
+
+function atualizaLista(gasto) {
+
+  const novoGasto = document.createElement('li');
+  novoGasto.classList.add('spent');
+  novoGasto.innerHTML = `
+    <img src="./img/${gasto.categoria}.svg" alt="Ícone de tipo da gasto" />
+
+    <div class="spent-info">
+      <strong>${gasto.nome}</strong>
+      <span>${gasto.categoria}</span>
+    </div>
+
+    <span class="spent-amount"><small>R$</small>${gasto.valor.replace('R$', '')}</span>
+
+    <img data-gastos="remover" src="./img/remove.svg" alt="remover" class="remove-icon" />
+  `;
+
+  lista.appendChild(novoGasto)
+  atualizaQuantidade();
+  atualizaTotal();
+}
+
+
+//------------ atualiza quantidade ------
+
+function atualizaQuantidade() {
+  const gastoDaLista = document.querySelectorAll('[data-gastos="lista"] li');
+
+  valorDaQuantidade.innerHTML = `${gastoDaLista.length} gastos`;
+}
+
+atualizaQuantidade();
+
+
+// -------------- atualiza total ---------
+
+function atualizaTotal() {
+
+  const valorGastado = document.querySelectorAll('[data-gastos="lista"] .spent-amount');
+
+  let soma = 0;
+
+  valorGastado.forEach((valor) => {
+    valor = valor.textContent.replace(/[^0-9,]/g, '').replace(',', '.')
+    valor = Number(valor);
+    soma += valor;
+  });
+
+  valorTotal.innerHTML = `<small>R$</small>${formatador.format(soma)}`
+}
+
+atualizaTotal();
+
+// ------------  remover gastos -----------
+
+function removerGasto() {
+
+  lista.addEventListener('click', (event) => {
+    if (event.target.matches('[data-gastos="remover"]')) {
+      const itemRemovido = event.target.closest('li');
+
+      itemRemovido.remove();
+
+      console.log('item removido')
+
+      atualizaQuantidade();
+      atualizaTotal();
+    }
+  })
+}
+
+removerGasto();
